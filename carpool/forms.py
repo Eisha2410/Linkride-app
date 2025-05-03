42f8ade55fc2250e1653b821bee67d6f325bad16
Eisha <eraazia496@gmail.com>
2025-05-03 22:14:24 +0500

    few more comments

--- carpool/forms.py
+++ carpool/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from .models import Ride
 
+# FRONTEND:
+# This form is used in the Django template (create_ride.html)
+# for creating a ride through the backend UI.
+
 class RideForm(forms.ModelForm):
     class Meta:
         model = Ride
